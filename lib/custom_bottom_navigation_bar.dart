040e0d5dfad39fa4d59b3b88b8f5f46a98ce32cc
Artemii <artemiithefrog95@icloud.com>
2024-05-10 17:13:32 +0200

    added cupertinoTabBar

---
+++ lib/custom_bottom_navigation_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onDestinationSelected;
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onDestinationSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabBar(
+      currentIndex: selectedIndex,
+      onTap: onDestinationSelected,
+      items: [
+        _buildNavigationBarItem(
+          context: context,
+          iconPath: 'lib/assets/icons/home.svg',
+          index: 0,
+        ),
+        _buildNavigationBarItem(
+          context: context,
+          iconPath: 'lib/assets/icons/search.svg',
+          index: 1,
+        ),
+        _buildNavigationBarItem(
+          context: context,
+          iconPath: 'lib/assets/icons/plus.svg',
+          index: 2,
+        ),
+        _buildNavigationBarItem(
+          context: context,
+          iconPath: 'lib/assets/icons/heart.svg',
+          index: 3,
+        ),
+        _buildNavigationBarItem(
+          context: context,
+          iconPath: 'lib/assets/icons/user.svg',
+          index: 4,
+        ),
+      ],
+    );
+  }
+
+  BottomNavigationBarItem _buildNavigationBarItem({
+    required BuildContext context,
+    required String iconPath,
+    required int index,
+  }) {
+    final isSelected = selectedIndex == index;
+
+    return BottomNavigationBarItem(
+      label: '',
+      icon: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: SvgPicture.asset(
+          iconPath,
+          color: isSelected ? Colors.black : Colors.grey,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:lookin_empat/custom_bottom_navigation_bar.dart';
+import 'package:lookin_empat/screens/add_new_look_screen.dart';
+import 'package:lookin_empat/screens/feed_screen.dart';
+import 'package:lookin_empat/screens/liked_looks_screen.dart';
+import 'package:lookin_empat/screens/profile_screen.dart';
+import 'package:lookin_empat/screens/search_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> screens = [
+    const FeedScreen(),
+    const SearchScreen(),
+    const AddNewLookScreen(),
+    const SavedLooksScreen(),
+    const ProfileScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: IndexedStack(
+          index: _currentIndex,
+          children: screens,
+        ),
+        bottomNavigationBar: CustomBottomNavigationBar(
+          selectedIndex: _currentIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
         ),
       ),
     );
